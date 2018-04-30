0827148d5319038c9ec7d50f2c67e48756be6f42
Albert Lardizabal <albie06@gmail.com>
2018-04-29 23:35:20 -0400

    Works on HTC Vive

---
+++ Assets/Scripts/ControllerInputManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInputManager : MonoBehaviour
+{
+    public SteamVR_TrackedObject trackedObj;
+    public SteamVR_Controller.Device device;
+
+    // Teleporation
+    private LineRenderer laser;
+    public GameObject teleportAimerObject;
+    public Vector3 teleportLocation;
+    public GameObject player;
+    public LayerMask laserMask;
+    public float yNudgeAmount = 1f; // specific to teleportAimerObject height
+
+    // Walking
+    public Transform playerCam;
+    public float moveSpeed = 4f;
+    private Vector3 movementDirection;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Grip))
+        {
+            movementDirection = playerCam.transform.forward;
+            movementDirection = new Vector3(movementDirection.x, 0, movementDirection.z);
+            movementDirection = movementDirection * moveSpeed * Time.deltaTime;
+            player.transform.position += movementDirection;
+        }
+        else
+        {
+            if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                laser.gameObject.SetActive(true);
+                teleportAimerObject.SetActive(true);
+                laser.SetPosition(0, gameObject.transform.position);
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.forward, out hit, 15, laserMask))
+                {
+                    teleportLocation = hit.point;
+                    laser.SetPosition(1, teleportLocation);
+                    // aimer position
+                    teleportAimerObject.transform.position = new Vector3(teleportLocation.x, teleportLocation.y + yNudgeAmount, teleportLocation.z);
+                }
+                else
+                {
+                    teleportLocation = new Vector3(transform.forward.x * 15 + transform.position.x, transform.forward.y * 15 + transform.position.y, transform.forward.z * 15 + transform.position.z);
+                    RaycastHit groundRay;
+                    if (Physics.Raycast(teleportLocation, -Vector3.up, out groundRay, 17, laserMask))
+                    {
+                        teleportLocation = new Vector3(transform.forward.x * 15 + transform.position.x, groundRay.point.y, transform.forward.z * 15 + transform.position.z);
+                    }
+                    laser.SetPosition(1, transform.forward * 15 + transform.position);
+                    // aimer position
+                    teleportAimerObject.transform.position = teleportLocation + new Vector3(0, yNudgeAmount, 0);
+                }
+            }
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                laser.gameObject.SetActive(true);
+                teleportAimerObject.SetActive(true);
+                //player.transform.position = teleportLocation;
+            }
+        }
+    }
+}
