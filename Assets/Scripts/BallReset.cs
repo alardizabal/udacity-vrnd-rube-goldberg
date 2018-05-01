0765b8b6d4419b63f7eebd722cab66cb65e57a1d
Albert Lardizabal <albie06@gmail.com>
2018-05-01 00:46:07 -0400

    Added ball reset code

--- Assets/Scripts/BallReset.cs
+++ Assets/Scripts/BallReset.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class BallReset : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject pedestal;
+
+    private Transform ballResetPosition;
+
+    //private LevelProgressionManager levelProgressionManager;
+
+    void Start()
+    {
+        for (int i = 0; i < this.pedestal.transform.childCount; i++)
+        {
+            Transform child = this.pedestal.transform.GetChild(i);
+            if (child.name == "BallResetPosition")
+            {
+                this.ballResetPosition = child.transform;
+                break;
+            }
+        }
+
+        //this.levelProgressionManager = this.GetComponent<LevelProgressionManager>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            Rigidbody ballRigidBody = this.gameObject.GetComponent<Rigidbody>();
+            ballRigidBody.velocity = Vector3.zero;
+            ballRigidBody.angularVelocity = Vector3.zero;
+
+            this.gameObject.transform.position = this.ballResetPosition.position;
+
+            //this.levelProgressionManager.ResetLevelProgression();
+        }
+    }
 }
